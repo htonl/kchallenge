fdd50b58905bf56c0de90e5ba1599004ec7859de
Luke Cesarz <lcesarz@gmail.com>
2018-08-25 18:14:11 -0500

    Finished t05. Doesn't work with gaming KB

--- t05/hello.c
+++ t05/hello.c
@@ -5,23 +5,9 @@
  */
 #include <linux/module.h>
 #include <linux/kernel.h>
-
-int init_module(void)
-{
-	static struct usb_device_id hello_ids[] = {
-	    {.driver_info = 42},
-	    {}
-	};
-
-	static struct usb_driver hello_driver = {
-	    .owner = THIS_MODULE,
-	    .name = "Flashy keyboard",
-	    .id_table = hello_ids,
-	    .probe = hello_probe,
-	    .disconnect = hello_disconnect,
-	};
-	return 0;
-}
+#include <linux/usb.h>
+#include <linux/slab.h>
+#include <linux/hid.h>
 
 int hello_probe(struct usb_interface *intf, const struct usb_device_id *id)
 {
@@ -29,15 +15,43 @@ int hello_probe(struct usb_interface *intf, const struct usb_device_id *id)
 	return 0;
 }
 
-void disconnect(struct usb_interface *intf)
+void hello_disconnect(struct usb_interface *intf)
 {
 	printk("unloading the flashy keyboard.\n");
 }
 
+static struct usb_device_id hello_ids[] = {
+	{USB_INTERFACE_INFO
+	(USB_INTERFACE_CLASS_HID, USB_INTERFACE_SUBCLASS_BOOT,
+	 USB_INTERFACE_PROTOCOL_KEYBOARD)},
+	{},
+};
+
+struct usb_driver hello_driver = {
+		.name = THIS_MODULE->name,
+    		.id_table = hello_ids,
+    		.probe = hello_probe,
+    		.disconnect = hello_disconnect,
+};
+
+int init_module(void)
+{
+	int result;
+    	printk("USB module loaded\n");
+	result = usb_register(&hello_driver);
+	if (result) 
+	    printk(KERN_ALERT "usb_register failed, error %d\n",result);
+
+	return result;
+}
+
+
 void cleanup_module(void)
 {
+    printk(KERN_ALERT "Tearing down module\n");
+    usb_deregister(&hello_driver);
 }
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Luke Cesarz");
-MODULE_DSCRIPTION("Just a helloW module");
+MODULE_DESCRIPTION("Just a helloW module");
