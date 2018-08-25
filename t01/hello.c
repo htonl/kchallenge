c21a093274d6d2da19c440e95625bba10fa1c0b5
Luke Cesarz <lcesarz@gmail.com>
2018-08-25 15:05:19 -0500

    Task01 done.
      * added hello.c

---
+++ t01/hello.c
@@ -0,0 +1,21 @@
+/*
+ * hello world module.
+ *
+ * Copyright (C) 2018 lcesarz
+ */
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void)
+{
+	printk(KERN_ALERT "Hello World!\n");
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_ALERT "Goodbye, Cruel World!\n");
+}
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Luke Cesarz");
+MODULE_DSCRIPTION("Just a helloW module");
